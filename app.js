a2ff999a78146df72dc37546e3699ac1471045fe
Joshua Schnell <joshua@opdemand.com>
2011-11-22 15:49:25 -0700

    Adding new handler for /myapp

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ app.get('/', function(req, res){
   res.send('Powered by OpDemand');
 });
 
+app.get('/myapp', function(req, res){
+  res.send('My application goes here');
+});
+
 app.listen(3000);
 
 console.log('Server listening on port %d in %s mode', app.address().port, app.settings.env);
